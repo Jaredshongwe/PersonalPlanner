48c565023985efc09dbb6cfb88b90b2558a7c0d2
Jaredshongwe <jaredshongwe@gmail.com>
2024-06-05 19:38:25 +0200

    Republished Project

--- obj/Release/net8.0/PersonalPlanner.AssemblyInfo.cs
+++ obj/Release/net8.0/PersonalPlanner.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonalPlanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a2cbb987702a4eadc303d5899d0180e512c7780")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cfe9c35ec6b5cf2837cf939dba507f826e2a1b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonalPlanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonalPlanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
